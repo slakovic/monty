adc69a3b2d47a90c947c0c25e6ea60a8c95f8726
slakovic <aladefm.o.a@gmail.com>
2022-12-20 04:35:17 +0100

    monty

--- main.c
+++ main.c
@@ -1,28 +1,45 @@
-
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - entry point of our monty program
- * @argc: the amount of arguments given to the program
- * @argv: the array of arguments given to the program
- *
- * Return: EXIT_SUCCESS if no error has occures, otherwise EXIT_FAILURE
- */
-
-int main(int argc, char **argv)
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
 {
-	FILE *fp;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	META.error = 0;
-	META.state = STACK;
-
-	if (handle_input(argc, argv, &fp) == 1 || monty_run(fp) == 1)
+	if (argc != 2)
 	{
-		if (fp)
-			fclose(fp);
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fclose(fp);
-	return (EXIT_SUCCESS);
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
